60bd7a8febe00ee4421e90fe5546aeecc48b7f08
Lotad <145983485+Lotading@users.noreply.github.com>
2024-02-19 08:53:35 +0000

    set port and ip addr (not usable yet)

--- server.c
+++ server.c
@@ -1,16 +1,26 @@
-#include <winsock2.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <windows.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 #define BUFFER 1024
 
-unsigned long WSAAPI inet_addr(
-  const char *cp    
-);
+struct sockaddr_in server_addr;
 
 int main(int argc, char** argv) {
-    inet_addr("127.0.0.1");
-
     
+    int socket_desc, client_sock, client_size[BUFFER];
+    struct sockaddr_in server_addr, client_addr;
+
+    socket_desc = socket(AF_INET,SOCK_STREAM,0);
+ 
+    if (socket_desc < 0) {
+        perror("its so over");
+        return -1;
+    }
+    printf("we are so back");
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8000);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 }
