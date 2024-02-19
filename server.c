68764c8026a651e90bca6a464d684a869da00b09
Lotad <145983485+Lotading@users.noreply.github.com>
2024-02-19 11:25:03 +0000

    added listen and bind, started on client

--- server.c
+++ server.c
@@ -9,7 +9,7 @@ struct sockaddr_in server_addr;
 
 int main(int argc, char** argv) {
     
-    int socket_desc, client_sock, client_size[BUFFER];
+    int socket_desc, client_sock, client_size;
     struct sockaddr_in server_addr, client_addr;
 
     socket_desc = socket(AF_INET,SOCK_STREAM,0);
@@ -23,4 +23,19 @@ int main(int argc, char** argv) {
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(8000);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (bind(socket_desc,(struct sockaddr*)&server_addr,sizeof(server_addr)) < 0)
+    {
+        perror("err bind failed");
+        return -1;
+    }
+    printf("bind succeded");
+
+    if (listen(socket_desc,1) < 0) {
+        perror("listen failed");
+        return -1;
+    }
+
+    client_size = sizeof(client_addr);
+    client_sock = accept(socket_desc,(struct sockaddr*)&client_addr,&client_addr)
 }
