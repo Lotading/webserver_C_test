cf09de725ca2c4352f27dd97481f3b076797407a
Lotad <145983485+Lotading@users.noreply.github.com>
2024-02-19 11:45:16 +0000

    finalised socket for client and server now launches

--- server.c
+++ server.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
 
 #define BUFFER 1024
 
@@ -11,14 +13,18 @@ int main(int argc, char** argv) {
     
     int socket_desc, client_sock, client_size;
     struct sockaddr_in server_addr, client_addr;
+    char server_message[BUFFER], client_message[BUFFER];
+
+    memset(server_message, '\0', sizeof(server_message));
+    memset(client_message, '\0', sizeof(client_message));
 
     socket_desc = socket(AF_INET,SOCK_STREAM,0);
  
     if (socket_desc < 0) {
-        perror("its so over");
+        perror("its so over\n");
         return -1;
     }
-    printf("we are so back");
+    printf("we are so back\n");
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(8000);
@@ -26,13 +32,13 @@ int main(int argc, char** argv) {
 
     if (bind(socket_desc,(struct sockaddr*)&server_addr,sizeof(server_addr)) < 0)
     {
-        perror("err bind failed");
+        perror("err bind failed\n");
         return -1;
     }
-    printf("bind succeded");
+    printf("bind succeded\n");
 
     if (listen(socket_desc,1) < 0) {
-        perror("listen failed");
+        perror("listen failed\n");
         return -1;
     }
 
@@ -45,5 +51,21 @@ int main(int argc, char** argv) {
     }
     printf("IP: %s & PORT: %i",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
 
+    if (recv(client_size,client_message,strlen(server_message),0) < 0) {
+        perror("message does not respect buffer boundaries >:( \n");
+        return -1;
+    }
+    printf("Msg from client: %s\n", client_message);
 
+    strcpy(server_message, "mesage from server\n");
+
+    if (send(client_sock,server_message,strlen(server_message),0) < 0)
+    {
+        perror("failed error thingy bleehhh\n");
+        return -1;
+    }
+    
+    close(socket_desc);
+    close(client_sock);
+    return 0;
 }
