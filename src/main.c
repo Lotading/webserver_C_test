76d5b5c1ff3e53bb1fb51286b4240e1f2bd075fc
Lotad <145983485+Lotading@users.noreply.github.com>
2024-02-20 14:14:29 +0000

    small changes wont specify :3

--- src/main.c
+++ src/main.c
@@ -1,14 +1,18 @@
 #include "header/main.h"
 #include "header/server.h"
 
+#include <stdlib.h>
 #include <stdio.h>
 
 int main() {
     printf("hello world!\n");
-    int sock = wSock();
-    while (true)
+    int _sock = wSock();
+    while (_sock)
     {
-       sock;
+       if (_sock = -1)
+       {
+            perror("something went wrong");
+            exit(EXIT_FAILURE);
+       }
     }
-    
 }
--- src/server.c
+++ src/server.c
@@ -3,7 +3,7 @@
 struct sockaddr_in server_addr;
 
 int wSock() {
-    
+    // @Lotading: this is retarded what T_T
     socklen_t socket_desc, client_sock, client_size;
     struct sockaddr_in server_addr, client_addr;
     char server_message[BUFFER], client_message[BUFFER];
@@ -27,22 +27,19 @@ int wSock() {
     {
         perror("err bind failed\n");
         return -1;
-    }
-    printf("bind succeded\n");
+    } printf("bind succeded\n");
 
     if (listen(socket_desc,1) <= 0) {
         perror("listen failed\n");
         return -1;
-    }
-    printf("where connection man\n");
+    } printf("where connection man\n");
 
     client_size = sizeof(client_addr);
     client_sock = accept(socket_desc,(struct sockaddr*)&client_addr,&client_size);
     if (client_sock <= 0) {
         perror("client failed? might be too much my guy");
         return -1;
-    }
-    printf("IP: %s & PORT: %i",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
+    } printf("IP: %s & PORT: %i",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
 
     if (recv(client_size,client_message,strlen(server_message),0) <= 0) {
         perror("message does not respect buffer boundaries >:( \n");
