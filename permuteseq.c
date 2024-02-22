86edd7a98dc37ec24380eb77ce40c7ae1bcd546b
Lotad <145983485+Lotading@users.noreply.github.com>
2024-02-22 07:38:53 +0000

    svelte test folder

---
+++ permuteseq.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+    char *seq;
+} permuteseq;
+
+char *getPermutation(int n, int k) {
+    int iterations;
+
+    permuteseq *Permuteseq;  
+    Permuteseq->seq = malloc(Permuteseq);
+    char* pSeq = &Permuteseq->seq;
+
+    char *nStr = n;
+    for (size_t i = 0; i < n; i++)
+    {
+        nStr[i]
+    }
+    return Permuteseq->seq;
+}
